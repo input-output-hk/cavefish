18a9ae48e36d41cb5ac45d897fc914e0334ca235
kayvank <kayvan@q2io.com>
2025-11-25 18:40:08 -0800

    Add Configuration file
    Add TOML configuration file for cavefish-server

---
+++ cavefish-server/cavefish/core/src/Core/Api/Config.hs
@@ -0,0 +1,125 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingVia #-}
+
+-- | Module for loading and representing the application's configuration
+module Core.Api.Config where
+
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Data.Aeson (ToJSON)
+import Data.Default (Default (def))
+import Data.Text.IO qualified
+import GHC.Generics (Generic)
+import System.Exit (exitFailure)
+import Toml (decode)
+import Toml.Schema (
+  FromValue (fromValue),
+  GenericTomlTable (GenericTomlTable),
+  Result (Failure, Success),
+  ToTable,
+  ToValue,
+  parseTableFromValue,
+  reqKey,
+ )
+
+-- | Representation of the HTTP server configuration
+data HttpServer = HttpServer
+  { host :: String
+  , port :: Int
+  }
+  deriving (Show, Generic)
+  deriving (ToTable, ToValue, FromValue) via GenericTomlTable HttpServer
+
+instance ToJSON HttpServer
+
+instance Default HttpServer where
+  def =
+    HttpServer
+      { host = "0.0.0.0"
+      , port = 8080
+      }
+
+-- | Representation of the Weakly Blind Predicate Signature configuration
+newtype Wbps = Wbps
+  { path :: FilePath
+  }
+  deriving (Show, Generic)
+
+instance ToJSON Wbps
+
+instance FromValue Wbps where
+  fromValue = parseTableFromValue (Wbps <$> reqKey "path")
+
+instance Default Wbps where
+  def =
+    Wbps
+      { path = "wbps"
+      }
+
+-- | Representation of the Service Provider, SP, fee configuration
+newtype ServiceProviderFee = ServiceProviderFee
+  { amount :: Integer
+  }
+  deriving (Show, Generic)
+
+instance ToJSON ServiceProviderFee
+
+instance FromValue ServiceProviderFee where
+  fromValue = parseTableFromValue (ServiceProviderFee <$> reqKey "amount")
+
+instance Default ServiceProviderFee where
+  def =
+    ServiceProviderFee
+      { amount = def
+      }
+
+-- | Representation of the transaction expiry configuration
+newtype TransactionExpiry = TransactionExpiry
+  { seconds :: Integer
+  }
+  deriving (Show, Generic)
+
+instance ToJSON TransactionExpiry
+
+instance FromValue TransactionExpiry where
+  fromValue = parseTableFromValue (TransactionExpiry <$> reqKey "seconds")
+
+instance Default TransactionExpiry where
+  def =
+    TransactionExpiry
+      { seconds = 3600
+      }
+
+-- | Representation of the overall application configuration
+data Config = Config
+  { httpServer :: HttpServer
+  , wbps :: Wbps
+  , serviceProviderFee :: ServiceProviderFee
+  , transactionExpiry :: TransactionExpiry
+  }
+  deriving (Show, Generic)
+  deriving (FromValue) via GenericTomlTable Config
+
+instance ToJSON Config
+
+instance Default Config where
+  def =
+    Config
+      { httpServer = def
+      , wbps = def
+      , serviceProviderFee = def
+      , transactionExpiry = def
+      }
+
+-- | Load the configuration from a TOML file
+loadConfig :: MonadIO m => FilePath -> m Config
+loadConfig filename = do
+  txt <- liftIO $ Data.Text.IO.readFile filename
+  case decode txt of
+    Success _ cfg -> pure cfg
+    Failure err -> liftIO $ do
+      putStrLn $
+        "Failed to load config file: "
+          <> filename
+          <> "with error: "
+          <> show err
+      exitFailure
